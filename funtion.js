c7495ae116189c69c94f1413ac4a2d42e47d7546
11111Asif <asif040502@gmail.com>
2025-09-10 23:36:45 -0700

    added to word count

--- funtion.js
+++ funtion.js
@@ -10,3 +10,16 @@ return count;
 }
 
 console.log(saraBocor("my name is rabbi? what is your name ! my name . akram "));
+
+function wordCount(boktrita) { 
+    let count = 0;
+    for(let i = 0; i < boktrita.length; i ++){
+        if(boktrita[i] == ' ' && boktrita[i - 1] != ' '){
+            count ++
+        }
+    }
+    count ++
+    return count;
+}
+
+console.log(wordCount('hello  word'))
